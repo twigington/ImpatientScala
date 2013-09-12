10e29c1d7aa74e512421d45886fc38c70fc101b3
Tom WIgington <tom.wigington@gmail.com>
2013-09-12 12:10:59 -0500

    further work on chapter 7

--- Chapter7.scala
+++ Chapter7.scala
@@ -1,3 +1,7 @@
+/*
+Chapter 7 answers.
+*/
+
 /* 1: [Write an example program to demonstrate that
             package com.horstmann.impatient
     is not the same as
@@ -10,6 +14,7 @@
 /*
   2: Write a puzzler that baffles your Scala friends, using a package com that isn't at the top level.
 */
+// See chapter7.com
 
 /*
   3: Write a package random with functions nextInt(): Int, nextDouble(): Double, and setSeed(seed: Int): Unit.
@@ -28,16 +33,14 @@ println(random.nextDouble())
 /*
   4: Why do you think the Scala language designers provided the package object syntax instead of simply
   letting you add functions and variables to a package?
-
-  A: It is a limit of the JVM which prevented them from doing so.
 */
+//  A: It is a limit of the JVM which prevented them from doing so.
 
 /*
   5: What is the meaning of private[com] def giveRaise(rate: Double)? Is it useful?
-
-  A: It means anything in the com package would have access to call the function. It allows you to create things that
-  are only-package usable and not overridable with a sub-class
 */
+//  A: It means anything in the com package would have access to call the function.
+//  It allows you to create things that are only-package usable and not overridable with a sub-class.
 
 /*
   6: Write a program that copies all elements from a Java hash map into a Scala has map. Use imports to rename both classes.
@@ -60,4 +63,21 @@ new chapter7.Exercise7().run()
 // specify. Also java._ is already included by default and then partially overridden by the scala._ auto-import.
 
 /*
+  9: Write a program that imports the java.lang.System class, reads the user name from user.name system property,
+  reads a password from the Console object, and prints a message to the standard error stream if the password is not
+  "secret". Otherwise print a greeting to the standard output stream. Do not use any other imports, and do not use
+  any qualified names (with dots).
 */
+
+/*
+  10: Apart from StringBuilder, what other members of java.lang does the scala package override?
+*/
+scala.BigDecimal
+scala.BigInt
+scala.Boolean
+scala.Iterable
+scala.Iterator
+scala.Long
+scala.Stream
+scala.StringBuilder
+scala.Vector
