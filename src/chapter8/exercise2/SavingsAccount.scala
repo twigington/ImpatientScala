7b75b6d53227be914e5f56fbad353b39e3998ead
Tom WIgington <tom.wigington@gmail.com>
2013-11-27 15:03:18 -0600

    finish chapter 7 and start chapter 8

---
+++ src/chapter8/exercise2/SavingsAccount.scala
@@ -0,0 +1,10 @@
+package chapter8.exercise2
+
+/*
+2. Extend the BankAccount class of the preceding exercise into a class SavingsAccount that earns interest
+  every month (when a method earnMonthlyInterest is called) and has three free deposits or withdrawals every
+  month. Reset the transaction count in the earnMonthlyInterest method.
+ */
+class SavingsAccount {
+
+}
