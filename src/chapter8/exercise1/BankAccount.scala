8d16e196f1f10b2020f1eba75d99644c5305caf5
twigington <G14qRCwE>
2013-11-27 14:51:33 -0600

    Add Chapter 8 exercise 1.

---
+++ src/chapter8/exercise1/BankAccount.scala
@@ -0,0 +1,7 @@
+package chapter8.exercise1
+
+class BankAccount(initialBalance: Double) {
+  private var balance = initialBalance
+  def deposit(amount: Double) = { balance += amount; balance }
+  def withdraw(amount: Double) = { balance -= amount; balance }
+}
