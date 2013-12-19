bb8c34faee85c301f0190706ac3c020637557908
Tom WIgington <tom.wigington@gmail.com>
2013-12-19 11:04:08 -0600

    ch8e9 answers

--- src/chapter8/exercise9/Creature.scala
+++ src/chapter8/exercise9/Creature.scala
@@ -4,9 +4,12 @@ package chapter8.exercise9
 9. In the Creature class of Section 8.10, "Construction Order and Early Definitions", on page 92, replace val range
 with a def.
     What happens when you also use a def in the Ant subclass?
-        A: Things work as expected
+        A: Things work as expected. now that range is a function in both classes the constructor of the parent
+        class will call the overridden method of the child class and env will be initialized with 2 elements.
     What happens when you use a val in the subclass?
-        A: Compilation error that overriding value method needs to be stable, immutable value
+        A: When using a def in the superclass and a value in the subclass env is still initialized with zero elements
+        because the subclass variable is still an uninitialized zero and the superclass constructor appears to use
+        the value defined in the subclass before trying to use a function from itself.
     Why?
  */
 class CreatureA {
